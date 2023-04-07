267fa72908b5023e35b269c19e6698b150394a1b
Nishant Balaji <44332326+nishantbalaji@users.noreply.github.com>
2023-04-07 14:04:09 -0700

    Landing, Footer and About sections (#1)

---
+++ src/components/About/index.jsx
@@ -0,0 +1,23 @@
+import style from './style.module.scss';
+
+const About = () => (
+  <section className={style.container}>
+    <div className={style.content}>
+      <div className={style.aboutLeft}>
+        <img src="ece.jpg" alt="Placeholder" className={style.aboutImg} />
+      </div>
+      <div className={style.aboutRight}>
+        <h1>Join Us At ECE Day 2023</h1>
+        <p>
+          ECE Day is a celebration of the students and faculty members of the ECE Department at
+          UCSD. We’ll be having a surprise faculty speaker, tabling events, workshops, and socials
+          for you to attend. Students that attend events will earn raffle tickets, which can go
+          towards winning our prizes, which include speakers, keyboards, and a Nintendo Switch! We
+          look forward to seeing you there!
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default About;
