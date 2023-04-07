290a2df82f0e5f25e7f8605640143deb27dec7d9
JacksAwOLF <chiangochen@gmail.com>
2023-04-07 09:53:50 -0700

    show faq answer on click

--- src/components/FAQ/index.jsx
+++ src/components/FAQ/index.jsx
@@ -1,6 +1,71 @@
+import { useState } from 'react';
 import style from './style.module.scss';
 
-const FAQ = () => (
+export default function FAQ() {
+
+  const questions = [
+    {
+      q: "What is ECE Day?", 
+      a: "Good question",
+    },
+    {
+      q: "Who can attend?", 
+      a: "Anyone",
+    },
+    {
+      q: "What should I bring?",
+      a: "Yourself",
+    },
+    {
+      q: "When is it?",
+      a: "Next Thursday?",
+    },
+    {
+      q: "Does it cost money?",
+      a: "Nope",
+    },
+    {
+      q: "Can I join remotely?",
+      a: "No?",
+    },
+    {
+      q: "How do I sign up?",
+      a: "Just show up",
+    },
+    {
+      q: "Where is it?",
+      a: "Warren Bear",
+    }
+  ];
+  
+  const [showAns, setShowAns] = useState(new Array(questions.length).fill(false));
+
+  function handleClick(ind){
+    let newShowAns = new Array(questions.length).fill(false);
+    if (showAns[ind] == false)
+      newShowAns[ind] = true;
+    setShowAns(newShowAns);
+  }
+
+  function createRow(ind){
+    return (
+      <div className={style.faqRow} key={ind}>
+        <p className={style.faqText}>{questions[ind].q}</p>
+        <img 
+          src="faqPlus.svg" 
+          alt="Expand FAQ" 
+          className={style.faqPlus} 
+          onClick={()=>handleClick(ind)}
+          currentstate={showAns[ind] ? 1 : 0}
+        />
+        {showAns[ind] && 
+          <p className={style.faqAns}>{questions[ind].a}</p>
+        }
+      </div>
+    );
+  }
+
+  return (
   <section id="faq" className={style.container}>
     <div className={style.content}>
       <div className={style.header}>
@@ -9,18 +74,7 @@ const FAQ = () => (
 
       
       <div className={style.left}>
-        <div className={style.faqRow}>
-          <p className={style.faqText}>What is ECE Day?</p>
-          <img src="faqPlus.svg" alt="Expand FAQ" className={style.faqPlus} />
-        </div>
-        <div className={style.faqRow}>
-          <p className={style.faqText}>Who can attend?</p>
-          <img src="faqPlus.svg" alt="Expand FAQ" className={style.faqPlus} />
-        </div>
-        <div className={style.faqRow}>
-          <p className={style.faqText}>What should I bring?</p>
-          <img src="faqPlus.svg" alt="Expand FAQ" className={style.faqPlus} />
-        </div>
+        {[0, 1, 2, 3].map(x=>createRow(x))}
       </div>
 
       <div className={style.center}>
@@ -28,22 +82,9 @@ const FAQ = () => (
       </div>
 
       <div className={style.right}>
-      <div className={style.faqRow}>
-          <p className={style.faqText}>What is ECE Day?</p>
-          <img src="faqPlus.svg" alt="Expand FAQ" className={style.faqPlus} />
-        </div>
-        <div className={style.faqRow}>
-          <p className={style.faqText}>Who can attend?</p>
-          <img src="faqPlus.svg" alt="Expand FAQ" className={style.faqPlus} />
-        </div>
-        <div className={style.faqRow}>
-          <p className={style.faqText}>What should I bring?</p>
-          <img src="faqPlus.svg" alt="Expand FAQ" className={style.faqPlus} />
-        </div>
+        {[4, 5, 6, 7].map(x=>createRow(x))}
       </div>
     
     </div>  
-  </section>
-);
-
-export default FAQ;
+  </section>);
+}
