d6d78f8c26164ade319b61a2421df26d83e822f2
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-04-09 19:31:35 -0700

    Added eceday schedule

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -2,7 +2,11 @@ import style from './style.module.scss';
 
 const Schedule = () => (
   <section id="schedule" className={style.container}>
-    Schedule
+    <div className={style.fallenStar}>
+      <img className={style.starImage} src="Fallen_Star.svg" />
+    </div>
+    <div className={style.header}>Schedule</div>
+    <div className={style.calendarEvent}></div>
   </section>
 );
 
