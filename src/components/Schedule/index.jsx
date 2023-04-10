67a7c27facc90a8b51aa4078e7b09b7229008b80
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-04-09 21:02:44 -0700

    Added gcal fetch call

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -1,4 +1,20 @@
+import fetch from 'node-fetch';
 import style from './style.module.scss';
+
+const CAL_API = 'AIzaSyBbeJA33TZOj0WfZtv9DNC9Yxe6MrjWbLQ';
+const CAL_ID = 'eceday%40eng.ucsd.edu';
+const BASEPARAMS = `orderBy=startTime&singleEvents=true&timeMin=${new Date().toISOString()}`;
+const BASEURL = `https://www.googleapis.com/calendar/v3/calendars/${CAL_ID}/events?${BASEPARAMS}`;
+
+const getEvents = async () => {
+  const finalURL = `${BASEURL}${`&maxResults=${30}`}&key=${CAL_API}`;
+  try {
+    return (await fetch(finalURL)).json();
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
+};
 
 const Schedule = () => (
   <section id="schedule" className={style.container}>
